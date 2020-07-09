c35de76c9e27a2ef4fca8ae4c969a72e2c121e21
Arkadiusz Kawalec <arkadiusz.kawalec.programista@gmail.com>
2020-07-09 21:51:20 +0200

    Views.

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,41 +1,47 @@
 ﻿
+using System.Collections.Generic;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
         // GET: Movies/Random
-        public ActionResult Random()
+        public ViewResult Index()
         {
-            var movie = new Movie() {Name = "Shrek!" };
-            return View(movie);
+            var movies = GetMovies();
+
+            return View(movies);
         }
 
-        [Route("movies/realeased/{year}/{month:regex(\\d{2}):range(1, 12)}")]
-        public ActionResult ByReleaseDate(int year, int month)
+        private IEnumerable<Movie> GetMovies()
         {
-            return Content(year + "/" + month);
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
         }
 
-        /*
-        public ActionResult Edit(int id)
-        {
-            return Content("id=" + id);
+        public ActionResult Random() {
+
+            var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer> 
+            {
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
+            };
+
+            var viewModel = new RandomMovieViewModel 
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
         }
 
-        //movies
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-
-            if (string.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-
-            return Content(string.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
-        }
-        */
     }
 }
