5d9791da70d6a8c790c60f60d195c63f3a98aa4d
Arkadiusz Kawalec <arkadiusz.kawalec.programista@gmail.com>
2020-08-04 18:41:31 +0200

    Build a form for storing rentals.

--- Vidly/Controllers/Api/MoviesController.cs
+++ Vidly/Controllers/Api/MoviesController.cs
@@ -20,14 +20,22 @@ namespace Vidly.Controllers.Api
             _context = new ApplicationDbContext();
         }
 
-        public IHttpActionResult GetMovies()
+        public IHttpActionResult GetMovies(string query = null)
         {
-            var movieDtos = _context.Movies
+            var moviesQuery = _context.Movies
                 .Include(m => m.Genre)
+                .Where(m => m.NumberAvailable > 0);
+
+
+            if (!String.IsNullOrWhiteSpace(query))
+                moviesQuery = moviesQuery.Where(m => m.Name.Contains(query));
+
+            var moviesDtos = moviesQuery
                 .ToList()
                 .Select(Mapper.Map<Movie, MovieDto>);
 
-            return Ok(movieDtos);
+            return Ok(moviesDtos);
+
         }
 
         public IHttpActionResult GetMovie(int id)
